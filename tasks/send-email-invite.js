8b6cb6d1e0d21d2357f6cac58d090367473c84c3
Sam Schooler <git-commit@samschooler.com>
2024-03-13 16:18:43 -0600

    feat: fix issues with not actually looking up the right survey id

--- tasks/send-email-invite.js
+++ tasks/send-email-invite.js
@@ -9,20 +9,9 @@ module.exports = async function async(payload, helpers) {
   );
 
   const { id } = payload;
-
-  const { data: inviteData, error: inviteError } = await supabase
-    .from("invite")
-    .select("title,short_code")
-    .eq("survey", id)
-    .single();
-
-  if (inviteError) {
-    throw inviteError;
-  }
-
   const { data, error } = await supabase
     .from("survey_results")
-    .select("survey(screens),results")
+    .select("survey(id,screens),results")
     .eq("id", id)
     .single();
 
@@ -30,37 +19,55 @@ module.exports = async function async(payload, helpers) {
     throw error;
   }
 
-  if (data) {
-    const filledScreenKeys = Object.keys(data.results);
-    const emailScreenId = filledScreenKeys.filter(
-      (screenId) => data.survey.screens[screenId].type === "email"
-    );
-    const email = data.results[emailScreenId];
+  if (!data) {
+    helpers.logger.error(`No survey result found with id ${id}`);
+    return;
+  }
 
-    if (!email) {
-      return;
-    }
+  const { data: inviteData, error: inviteError } = await supabase
+    .from("invite")
+    .select("title,short_code")
+    .eq("survey", data.survey.id)
+    .single();
 
-    helpers.logger.info(
-      `New survey result created with id ${id} notifying ${email}!`
-    );
+  if (inviteError) {
+    throw inviteError;
+  }
 
-    const mailgun = new Mailgun(formData);
-    const mg = mailgun.client({
-      username: "api",
-      key: process.env.MAILGUN_API_KEY,
+  if (!inviteData) {
+    helpers.logger.error(`No invite found with survey id ${id}`);
+    return;
+  }
+
+  const filledScreenKeys = Object.keys(data.results);
+  const emailScreenId = filledScreenKeys.filter(
+    (screenId) => data.survey.screens[screenId].type === "email"
+  );
+  const email = data.results[emailScreenId];
+
+  if (!email) {
+    return;
+  }
+
+  helpers.logger.info(
+    `New survey result created with id ${id} notifying ${email}!`
+  );
+
+  const mailgun = new Mailgun(formData);
+  const mg = mailgun.client({
+    username: "api",
+    key: process.env.MAILGUN_API_KEY,
+  });
+  try {
+    await mg.messages.create("mail.littleinvite.com", {
+      from: "Little Invite <sam@mail.littleinvite.com>",
+      to: [email],
+      subject: `Invite to ${inviteData.title} has been RSVP'd!`,
+      text: `You RSVP'd to an invite! Go check it out at https://littleinvite.com/e/${inviteData.short_code}`,
     });
-    try {
-      await mg.messages.create("mail.littleinvite.com", {
-        from: "Little Invite <sam@mail.littleinvite.com>",
-        to: [email],
-        subject: `Invite to ${inviteData.title} has been RSVP'd!`,
-        text: `You RSVP'd to an invite! Go check it out at https://littleinvite.com/e/${inviteData.short_code}`,
-      });
-    } catch (e) {
-      helpers.logger.error(e);
+  } catch (e) {
+    helpers.logger.error(e);
 
-      throw e;
-    }
+    throw e;
   }
 };
